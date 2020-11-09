0cba0b8d8298e33ef1886737aec8c43b86a15471
hmd195 <hmd19570@gmail.com>
2020-11-09 21:27:38 +0100

    Na aflevering 12

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -1,26 +1,10 @@
 <div wire:init="loadComingSoon" class="most-anticipated-container space-y-10 mt-8">
     @forelse($comingSoon as $game)
-    <div class="game flex">
-        <a href="#">
-            <img src="{{ $game['coverImageUrl'] }}" alt="game cover"
-                class="w-16 hover:opacity-75 transition ease-in-out duration-150">
-        </a>
-        <div class="ml-4">
-            <a href="#" class="hoover:text-gray-300">{{ $game['name'] }}</a>
-            <div class="text-gray-400 text-sm mt-1">
-                {{ $game['first_release_date'] }}</div>
-        </div>
-    </div>
+        <x-game-card-small :game="$game" />
 
     @empty
-    @foreach(range(1,4) as $game)
-    <div class="game flex">
-        <div class="bg-gray-800 w-16 h-24"></div>
-        <div class="ml-4">
-            <div class="text-transparent bg-gray-700 rounded leading-tight">Name comeshere</div>
-            <div class="inline-block text-transparent bg-gray-700 rounded mt-4 text-sm">Sep 20,2020</div>
-        </div>
-    </div>
-    @endforeach
+        @foreach(range(1,4) as $game)
+            <x-game-card-small-skeleton />
+        @endforeach
     @endforelse
 </div>
